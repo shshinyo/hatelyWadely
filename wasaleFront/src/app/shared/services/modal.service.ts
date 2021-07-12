e3736f5e71dc862723a8bc189641e774697c8312
M-Hamo <58711682+M-Hamo@users.noreply.github.com>
2021-07-12 07:02:05 +0200

    Adding modal Serves

---
+++ wasaleFront/src/app/shared/services/modal.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from "@angular/core";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { Observable } from "rxjs";
+import { MatDialog } from "@angular/material/dialog";
+import { ConfirmDialogComponent } from "../components/confirm-dialog.component";
+
+export type SnackBarVariant = "normal" | "success" | "error";
+
+function _panelClass(variant: SnackBarVariant): string[] | undefined {
+  return variant === "success"
+    ? ["snack-success"]
+    : variant === "error"
+    ? ["snack-error"]
+    : undefined;
+}
+
+@Injectable({ providedIn: "root" })
+export class ModalService {
+  constructor(
+    private readonly _snackBar: MatSnackBar,
+    private readonly _dialog: MatDialog
+  ) {}
+
+  snackbar(message: string, variant?: SnackBarVariant, duration = 10000): void {
+    this._snackBar.open(message, "إغلاق", {
+      direction: "rtl",
+      duration,
+      panelClass: _panelClass(variant),
+    });
+  }
+
+  confirmDialog(message: string, title = "هل انت متأكد"): Observable<boolean> {
+    const ref = this._dialog.open(ConfirmDialogComponent, {
+      data: { title: title, message },
+      width: "500px",
+      direction: "rtl",
+      closeOnNavigation: true,
+    });
+
+    return ref.afterClosed();
+  }
+}
