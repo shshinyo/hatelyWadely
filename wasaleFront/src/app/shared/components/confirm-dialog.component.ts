4e56481f6d5d21537b8f66a680b9caf0a055049a
M-Hamo <58711682+M-Hamo@users.noreply.github.com>
2021-07-12 07:03:11 +0200

    Adding Confirming dialog 🚀

---
+++ wasaleFront/src/app/shared/components/confirm-dialog.component.ts
@@ -0,0 +1,30 @@
+import { Component, Inject } from "@angular/core";
+import { MAT_DIALOG_DATA } from "@angular/material/dialog";
+
+export interface IDialogContent {
+  /** Dialog title. */
+  readonly title: string;
+  /** Dialog message - can contain markup. */
+  readonly message: string;
+}
+
+@Component({
+  template: `
+    <h1 mat-dialog-title class="text-center text-gray-800">
+      <mat-icon style="font-size:20px;">arrow_back_ios_new</mat-icon>
+      {{ data.title }}
+    </h1>
+
+    <div mat-dialog-content class="m-y-8 h-t-18" [innerHTML]="data.message"></div>
+
+    <div mat-dialog-actions>
+      <button mat-button [mat-dialog-close]="true" aria-label="Ok">موافق</button>
+      <button mat-flat-button color="warn" [mat-dialog-close]="false" aria-label="Cancel">
+        الغاء
+      </button>
+    </div>
+  `,
+})
+export class ConfirmDialogComponent {
+  constructor(@Inject(MAT_DIALOG_DATA) public readonly data: IDialogContent) {}
+}
