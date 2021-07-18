418a320d93b7f6f4cd7833de33feec30fbc9f6d0
M-Hamo <58711682+M-Hamo@users.noreply.github.com>
2021-07-18 16:34:30 +0200

    until now fine 🚀

--- wasaleFront/src/app/pages/shop/shop-routing.module.ts
+++ wasaleFront/src/app/pages/shop/shop-routing.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { CardDataComponent } from "../souq/card-data/card-data.component";
+import { CategoryComponent } from "./category/category.component";
+import { ProductDetailComponent } from "./product-detail/product-detail.component";
 import { ProductsComponent } from "./products/products.component";
 
 const routes: Routes = [
   {
     path: "",
     component: ProductsComponent,
-    
   },
+  {
+    path: "categoryId",
+    component: CategoryComponent,
+  },
+  // {
+  //   path:":categoryId/:categoryType",
+  //   component:CategoryComponent
+  // }
+  // {
+  //   path:":categoryId/:categoryTypeId/:productId",
+  //   component:CategoryComponent
+  // }
 ];
 
 @NgModule({
--- wasaleFront/src/app/pages/shop/shop.module.ts
+++ wasaleFront/src/app/pages/shop/shop.module.ts
@@ -1,19 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { ShopRoutingModule } from './shop-routing.module';
-import { SharedModule } from 'src/app/shared/shared.module';
-import { ProductsComponent } from './products/products.component';
-
+import { ShopRoutingModule } from "./shop-routing.module";
+import { SharedModule } from "src/app/shared/shared.module";
+import { ProductsComponent } from "./products/products.component";
+import { ProductDetailComponent } from "./product-detail/product-detail.component";
+import { CategoryComponent } from "./category/category.component";
 
 @NgModule({
-  declarations: [
-    ProductsComponent
-  ],
-  imports: [
-    CommonModule,
-    SharedModule,
-    ShopRoutingModule
-  ]
+  declarations: [ProductsComponent, ProductDetailComponent, CategoryComponent],
+  imports: [CommonModule, ShopRoutingModule, SharedModule],
 })
-export class ShopModule { }
+export class ShopModule {}
